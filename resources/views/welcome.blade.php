79a8351d86d1e66953d5dccac8390106043d1c6b
Rena Salazar <renasalazart@outlook.com>
2022-07-22 21:00:02 -0600

    Se agregaron los recursos como fuentes y colores personalizados al proyecto via tailwind.config.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,9 +8,31 @@
 </head>
 
 <body>
-    <h1 class="text-3xl font-bold text-green-900 mt-10 text-center">
+    <h1 class="text-4xl font-bold text-green-900 mt-10 text-center">
         Hello world!
     </h1>
+
+    <h2 class="text-1xl text-center mt-5 text-blue-500 font-workSans font-light px-4">Lorem ipsum dolor, sit amet
+        consectetur
+        adipisicing
+        elit. Omnis
+        sapiente nulla sed
+        provident recusandae! Vero.</h2>
+    <h2 class="text-2xl text-center mt-5 text-orange-500 font-mavenPro font-bold uppercase px-4">Lorem ipsum dolor, sit
+        amet
+        consectetur
+        adipisicing
+        elit. Omnis
+        sapiente nulla sed
+        provident recusandae! Vero.</h2>
+
+    <div class="container bg-main w-12 m-10 h-15 text-white text-center p-2">Main</div>
+    <div class="container bg-links w-12 m-10 h-15 text-white text-center p-2">Main</div>
+    <div class="container bg-contrast w-12 m-10 h-15 text-white text-center p-2">Main</div>
+    <div class="container bg-contrast2 w-12 m-10 h-15 text-white text-center p-2">Main</div>
+    <div class="container bg-button w-12 m-10 h-15 text-white text-center p-2">Main</div>
+    <div class="container bg-contrast3 w-12 m-10 h-15 text-white text-center p-2">Main</div>
+    <div class="container bg-bars w-12 m-10 h-15 text-white text-center p-2">Main</div>
 </body>
 
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,17 @@ module.exports = {
         extend: {
             fontFamily: {
                 sans: "Montserrat",
+                workSans: "Work Sans",
+                mavenPro: "Maven Pro",
+            },
+            colors: {
+                main: "#009335",
+                button: "#00CB54",
+                contrast: "#1E2022",
+                contrast2: "#191919",
+                contrast3: "#7D797A",
+                links: "#BABABA",
+                bars: "#736549",
             },
         },
     },
